5df8a5f0f0c4603a0fb9d90401e736f3464f9e54
LonnonjamesD <lemonjameslem@protonmail.com>
2021-10-21 21:43:30 -0400

    Changed to a counter

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ local io = require("io")
 
 local generatedjson = "{\"blueprint\": {\"icons\": [{\"signal\": {\"type\": \"item\",\"name\": \"decider-combinator\"},\"index\": 1}],\"entities\": ["
 local position = {0, 0}
-local circuits = {}
+local circuits = 0
 
 function decode()
     local blueprint = "eNqFkdFqwzAMRf9Fz/ZY23TdTF/3FWMYJ1FbQWIbWw4Lwf8+2x1jMNgedX11dCVv0E8JfSDLoDagwdkI6m2DSFdrpqrx6hEUEOMMAqyZazXiQCMGObi5J2vYBcgCyI74AWqX3wWgZWLCO60Vq7Zp7jEUw18cAd7F0upsnV5wh93DUcAKSh7LjJKQg5t0jzezUPEX0xdFl7exdcaqXihE1v8sMrkrRaZBDjeMLL2JkRaUPrilIutWESv3N2ihwKko36y7Q75Cizl7E9pCCs5QBb+WgMmyvgQ3a7I+lZtzSJhzPVjLpX78h4AFQ2x32D/vutPL/tR1h8fuqcv5E8oCmCE="
@@ -31,9 +31,6 @@ function generate()
 end
 
 function decider(statement, output, connections)
-    local circuitslen = 0
-    for _ in pairs(circuits) do circuitslen = circuitslen + 1 end
-
     local splitstatement = {}
     for i in string.gmatch(statement, "%S+") do
         table.insert(splitstatement, i)
@@ -42,6 +39,7 @@ function decider(statement, output, connections)
     local comparator = splitstatement[2]
     local second = splitstatement[3]
     local outputsignal = ""
+    -- Chekcs if any of the inputs are constants or strings and them puts them accordingly
     if type(first) == "number" then 
         first = first
     elseif type(first) == "string" then
@@ -57,10 +55,12 @@ function decider(statement, output, connections)
     elseif type(output) == "string" then
         outputsignal = "{\"type\": \"virtual\",\"name\": \"signal-" .. output .. "\"},"
     end
-    if circuitslen == 0 then
-        generatedjson = generatedjson .. "{\"entity_number\":" .. circuitslen + 1 .. ",\"name\": \"decider-combinator\",\"position\": { \"x\": " .. position[1] .. ", \"y\": " .. position[2] .. "},\"control_behavior\": {\"decider_conditions\": {\"first_signal\": " .. first .. "\"second_signal\": " .. second .. "\"comparator\": \"" .. comparator .. "\",\"output_signal\":" .. outputsignal .. "\"copy_count_from_input\": true}}}" 
+    if circuits == 0 then
+        generatedjson = generatedjson .. "{\"entity_number\":" .. circuits + 1 .. ",\"name\": \"decider-combinator\",\"position\": { \"x\": " .. position[1] .. ", \"y\": " .. position[2] .. "},\"control_behavior\": {\"decider_conditions\": {\"first_signal\": " .. first .. "\"second_signal\": " .. second .. "\"comparator\": \"" .. comparator .. "\",\"output_signal\":" .. outputsignal .. "\"copy_count_from_input\": true}}}" 
     else
-        generatedjson = generatedjson .. "," .. "{\"entity_number\":" .. circuitslen + 1 .. ",\"name\": \"decider-combinator\",\"position\": { \"x\": " .. position[1] .. ", \"y\": " .. position[2] .. "},\"control_behavior\": {\"decider_conditions\": {\"first_signal\": " .. first .. "\"second_signal\": " .. second .. "\"comparator\": \"" .. comparator .. "\",\"output_signal\":" .. outputsignal .. "\"copy_count_from_input\": true}}}" 
+        generatedjson = generatedjson .. "," .. "{\"entity_number\":" .. circuits + 1 .. ",\"name\": \"decider-combinator\",\"position\": { \"x\": " .. position[1] .. ", \"y\": " .. position[2] .. "},\"control_behavior\": {\"decider_conditions\": {\"first_signal\": " .. first .. "\"second_signal\": " .. second .. "\"comparator\": \"" .. comparator .. "\",\"output_signal\":" .. outputsignal .. "\"copy_count_from_input\": true}}}" 
     end
+    circuits = circuits + 1
+    position[1] = position[1] + 1
     return {1}
 end
