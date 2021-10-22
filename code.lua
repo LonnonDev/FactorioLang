ba1802785271d7e0fbf438ecf08a091fe876a678
LonnonjamesD <lemonjameslem@protonmail.com>
2021-10-21 21:43:36 -0400

    More Examples

--- code.lua
+++ code.lua
@@ -2,6 +2,8 @@ require("main")
 
 -- Args: Statement, Output, Wires
 local basic = decider("E > B", "C", {})
-
+local basic2 = decider("E > B", "C", {})
+local basic3 = decider("E > 2", "C", {})
+local basic4 = decider("1 > B", "C", {})
 
 generate()
